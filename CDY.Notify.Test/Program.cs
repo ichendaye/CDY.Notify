ca7c7695afe1e304af42dd218b20997ecb94f292
cc <admin@chendaye.com>
2024-08-05 18:22:56 +0800

    1.0.0.1

---
+++ CDY.Notify.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CDY.Notify.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var notify = new DingTalk("测试",
+                                          "https://oapi.dingtalk.com/robot/send?access_token=dbbf5c71ee1fede0a32f5be392487778888fd0f9647a10fb6cc3fd4988c4fa3b",
+                                          "SECdf96006fc63a5bd1522dde80e5a00bbb33ed58b8a2eb14a330d71e0f41e632f7");
+                //_ = notify.SendTextMsgAsync("123");
+                _ = notify.SendErrorMsgAsync(new Exception("gagaga"),true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- CDY.Notify/DingTalk.cs
+++ CDY.Notify/DingTalk.cs
@@ -1,4 +1,7 @@
 ﻿using RestSharp;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
 
 namespace CDY.Notify
 {
@@ -7,15 +10,21 @@ namespace CDY.Notify
     /// </summary>
     public class DingTalk
     {
-        static RestClient client;
+        RestClient client = null;
+        string AppName = string.Empty;
+        string Secret = string.Empty;
 
         /// <summary>
         /// 初始化钉钉通知
         /// </summary>
+        /// <param name="appName">应用名称</param>
         /// <param name="webhook">webhook地址</param>
-        public DingTalk(string webhook)
+        /// <param name="secret">密钥</param>
+        public DingTalk(string appName, string webhook, string secret)
         {
             client = new RestClient(webhook);
+            AppName = appName;
+            Secret = secret;
         }
 
         /// <summary>
@@ -23,23 +32,119 @@ namespace CDY.Notify
         /// </summary>
         /// <param name="content">消息内容</param>
         /// <param name="isAtAll">是否@所有人</param>
-        public void SendTextMsg(string content, bool isAtAll = false)
+        public async Task SendTextMsgAsync(string content, bool isAtAll = false)
         {
+            if (client == null)
+            {
+                throw new Exception("钉钉通知未初始化");
+            }
+
             var body = new
             {
                 msgtype = "text",
                 text = new
                 {
-                    content
+                    content = $"【{AppName}】\r\n{content}" 
                 },
-                at=new
+                at = new { isAtAll }
+            };
+
+            RestRequest request = new RestRequest();
+            request.AddBody(body);
+            var timestamp = GenerateTimestamp();
+            request.AddQueryParameter("timestamp", timestamp);
+            request.AddQueryParameter("sign", GenerateSignature(timestamp));
+
+            try
+            {
+                // 等待异步操作完成，并处理可能的异常
+                var jg=await client.PostAsync(request);
+            }
+            catch (Exception ex)
+            {
+                // 处理发送消息时可能出现的异常，例如网络错误等
+                Console.WriteLine($"发送消息失败: {ex.Message}");
+                // 可以根据需要抛出异常或进行其他错误处理
+                throw; // 或者用其他方式处理异常，如记录日志等
+            }
+        }
+
+        /// <summary>
+        /// 发送异常消息
+        /// </summary>
+        /// <param name="exception">异常消息</param>
+        /// <param name="isAtAll">是否@所有人</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task SendErrorMsgAsync(Exception exception, bool isAtAll = false)
+        {
+            if (client == null)
+            {
+                throw new Exception("钉钉通知未初始化");
+            }
+            var body = new
+            {
+                msgtype = "markdown",
+                markdown = new
+                {
+                    title = $"【{AppName}】",
+                    text = $"# {AppName}异常信息\r\n```\r\n{exception.Message}\r\n```\r\n# 堆栈信息\r\n```\r\n{exception.StackTrace}\r\n```"
+                },
+                at = new
                 {
                     isAtAll
                 }
             };
             RestRequest request = new RestRequest();
             request.AddBody(body);
-            _ = client.PostAsync(request);
+            var timestamp = GenerateTimestamp();
+            request.AddQueryParameter("timestamp", timestamp);
+            request.AddQueryParameter("sign", GenerateSignature(timestamp));
+            try
+            {
+                await client.PostAsync(request); // 等待异步操作完成，并处理可能的异常
+            }
+            catch (Exception ex)
+            {
+                // 记录或处理发送通知时发生的异常，例如日志记录等。
+                Console.WriteLine($"发送钉钉通知失败: {ex.Message}");
+                // 可以根据需要重新抛出异常或进行其他处理。
+                throw; // 或者用其他方式处理异常，如记录日志等
+            }
+        }
+
+        /// <summary>
+        /// 生成时间戳
+        /// </summary>
+        /// <returns></returns>
+        private long GenerateTimestamp()
+        {
+            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalMilliseconds);
+        }
+
+        /// <summary>
+        /// 生成签名
+        /// </summary>
+        /// <returns></returns>
+        private string GenerateSignature(long timestamp)
+        {
+            // 将时间戳和密钥组合成签名字符串
+            string signString = $"{timestamp}\n{Secret}";
+
+            // 使用 HMACSHA256 算法生成签名
+            using (var hmacsha256 = new HMACSHA256(Encoding.UTF8.GetBytes(Secret)))
+            {
+                byte[] hashBytes = hmacsha256.ComputeHash(Encoding.UTF8.GetBytes(signString));
+
+                // 将签名结果进行Base64编码
+                string base64Signature = Convert.ToBase64String(hashBytes);
+
+                // 对Base64编码后的签名进行URL编码
+                string urlEncodedSignature = HttpUtility.UrlEncode(base64Signature);
+
+                return urlEncodedSignature;
+            }
         }
     }
 }
